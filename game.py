0d2b8a41fdb9f803b960516329d45223d8f247c1
Tim Greaves <tim@greavest.co.uk>
2015-07-04 09:02:15 +0000

    Some of the logic has been extracted into methods.

--- game.py
+++ game.py
@@ -3,24 +3,41 @@ import pygame
 
 class PlayerShip(object):
 
+    SPEED = 1
+
     def __init__(self, start_x, start_y):
         self._x = start_x
         self._y = start_y
 
     def move_up(self):
-        self._y -= 1
+        self._y -= PlayerShip.SPEED
 
     def move_down(self):
-        self._y += 1
+        self._y += PlayerShip.SPEED
 
     def move_left(self):
-        self._x -= 1
+        self._x -= PlayerShip.SPEED
 
     def move_right(self):
-        self._x += 1
+        self._x += PlayerShip.SPEED
 
     def get_position(self):
         return (self._x, self._y)
+
+
+def process_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit_game()
+
+def exit_game():
+    pygame.quit()
+    sys.exit()
+
+def update_screen():
+    screen.fill(black)
+    screen.blit(player_image, player.get_position())
+    pygame.display.update()
 
 
 
@@ -34,10 +51,7 @@ player_image = pygame.image.load("resources/PlayerShip.png")
 player = PlayerShip(0, 0)
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    process_events()
 
     pressed_keys = pygame.key.get_pressed()
     if pressed_keys[pygame.K_UP]:
@@ -49,6 +63,5 @@ while True:
     if pressed_keys[pygame.K_RIGHT]:
         player.move_right()
    
-    screen.fill(black)
-    screen.blit(player_image, player.get_position())
-    pygame.display.flip()
+    update_screen()
+

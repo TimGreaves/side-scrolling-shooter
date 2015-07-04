26ae47f5debd7a641f3dab049d2047afe577653c
Tim Greaves <tim@greavest.co.uk>
2015-07-04 08:19:07 +0000

    Added horizontal movement.

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ player_rect = player.get_rect()
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:            
             sys.exit()
 
     pressed_keys = pygame.key.get_pressed()
@@ -20,6 +20,10 @@ while True:
         player_rect = player_rect.move(0, -1)
     if pressed_keys[pygame.K_DOWN]:
         player_rect = player_rect.move(0, 1)
+    if pressed_keys[pygame.K_LEFT]:
+        player_rect = player_rect.move(-1, 0)
+    if pressed_keys[pygame.K_RIGHT]:
+        player_rect = player_rect.move(1, 0)
     
     screen.fill(black)
     screen.blit(player, player_rect)
